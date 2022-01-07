a9aaef7ac887c5747423c666c267ee356c47ff5d
Trevleon <trev.leon7@gmail.com>
2022-01-07 17:12:22 -0600

    archon budget changes

--- src/sprintbot/Archon.java
+++ src/sprintbot/Archon.java
@@ -7,6 +7,7 @@ import battlecode.common.RobotType;
 
 public class Archon {
     public static void run(RobotController rc) throws GameActionException {
+        int archonCount = rc.getArchonCount();
         Communication.updateFood(rc);
 
         for (int x = 0; x < 16; x++) {
@@ -20,13 +21,32 @@ public class Archon {
         if (rc.getRoundNum() < 20) {
             Utils.tryBuild(RobotType.MINER, rc);
         } else if (rc.getRoundNum() < 100 && rc.getTeamLeadAmount(rc.getTeam()) >= RobotType.SOLDIER.buildCostLead) {
-            if (Utils.rng.nextInt(3) == 0) {
-                Utils.tryBuild(RobotType.SOLDIER, rc);
+            if (Utils.rng.nextInt(2) == 0) {
+                if(rc.getTeamLeadAmount(rc.getTeam()) / archonCount > RobotType.SOLDIER.buildCostLead)
+                    Utils.tryBuild(RobotType.SOLDIER, rc);
             } else {
-                Utils.tryBuild(RobotType.MINER, rc);
+                if(rc.getTeamLeadAmount(rc.getTeam()) / archonCount > RobotType.MINER.buildCostLead)
+                    Utils.tryBuild(RobotType.MINER, rc);
             }
         } else if (rc.getTeamLeadAmount(rc.getTeam()) >= RobotType.WATCHTOWER.buildCostLead) {
-            Utils.tryBuild(Utils.leadDroidTypes[Utils.rng.nextInt(Utils.leadDroidTypes.length)], rc);
+            //RobotType droidType = Utils.leadDroidTypes[Utils.rng.nextInt(Utils.leadDroidTypes.length)];
+            if(Communication.getDroidCount(RobotType.SOLDIER) < 10) {
+                Utils.tryBuild(RobotType.SOLDIER, rc);
+            }
+            else if(Communication.getDroidCount(RobotType.MINER) < 10) {
+                Utils.tryBuild(RobotType.MINER, rc);
+            }
+            else if (Communication.getDroidCount(RobotType.BUILDER) < 10) {
+                Utils.tryBuild(RobotType.BUILDER, rc);
+            }
+            else {
+                RobotType randDroidType = Utils.leadDroidTypes[Utils.rng.nextInt(Utils.leadDroidTypes.length)];
+                if(rc.getTeamLeadAmount(rc.getTeam()) / archonCount > randDroidType.buildCostLead) {
+                    Utils.tryBuild(randDroidType, rc);
+                }
+
+            }
+          
         }
     }
 }
